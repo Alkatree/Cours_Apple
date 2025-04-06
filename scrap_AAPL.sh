641f36352f4b91bfc16ad0c0b1322db9ea53193e
Alkatree <149832008+Alkatree@users.noreply.github.com>
2025-04-06 22:43:30 +0200

    Update scrap_AAPL.sh

--- scrap_AAPL.sh
+++ scrap_AAPL.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Définir la clé d'API et le symbole de l'indice
+# Clé API et symbole Apple
 API_KEY="cvpde31r01qve7io8q5gcvpde31r01qve7io8q60"
 SYMBOL="AAPL"
 
-# URL de l'API Finnhub pour récupérer le cours actuel
+# URL de l'API Finnhub 
 URL="https://finnhub.io/api/v1/quote?symbol=$SYMBOL&token=$API_KEY"
 
-# Fichier de sortie pour stocker les données
+# Fichier CSV pour les datas
 OUTPUT_FILE="AAPL_data.csv"
 
-# Vérifier si le fichier de sortie existe, sinon créer le header
+# Vérifier si le fichier de sortie existe
 if [ ! -f "$OUTPUT_FILE" ]; then
     echo "timestamp,price" > "$OUTPUT_FILE"
 fi
@@ -23,9 +23,7 @@ current_price=$(echo "$response" | jq '.c')
 
 # Vérifier si le prix est valide
 if [ "$current_price" != "null" ]; then
-    # Obtenir le timestamp actuel
     timestamp=$(date --iso-8601=seconds)
-    # Enregistrer les données dans le fichier CSV
     echo "$timestamp,$current_price" >> "$OUTPUT_FILE"
     echo "Données enregistrées : $timestamp, $current_price"
 else
